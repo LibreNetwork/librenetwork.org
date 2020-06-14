7a4d34080cbe4254c3a7560a59c64532186864ee
Travis Newton <Travis.Newton@xinsto.com>
2020-06-13 22:25:07 -0400

    ORL -> LibreNetwork plus fixes
    Removed legacy Orlando Meshnet to LibreNetwork, fixed about page, added last modfied date to site footer.

--- about.php
+++ about.php
@@ -15,7 +15,14 @@ include("navbar.php");
       <div class="page-header">
         <h1>About Us</h1>
       </div>
-      <p>Welcome to the LibreNetwork Project! We're here to help build an open Internet - a truely global community that is not restricted by expensive ISPs, prone to censorship, and the like.</p>
+      <p>Welcome to the LibreNetwork Project! We're here to help build an open Internet - a truely global community that is not restricted by expensive ISPs, prone to censorship, and the like. Originally, LibreNetwork
+      started off as The Orlando Meshnet project - a subdivision of a proposed meshnet project that had roots on reddit back in 2012 (and you'll find references still in our wiki history). This gained popularity because of the 
+      <a href="https://en.wikipedia.org/wiki/Stop_Online_Piracy_Act" rel="nofollow" target="_blank">SOPA</a>. In a nutshell, it was written to allow anyone to essentially censor the internet. Thankfully, the bill failed.</p>
+      <p>Around the same time, the director of LibreNetwork, Travis Newton, was also volunteering with The Free Network Foundation (The FNF). The FNF has become defunct, sadly, however, this is where LibreNetwork comes in. 
+      We, in the spirit of open source, have "forked" The FNF. We want to continue to build upon the great work Issac, Charles, Tyrone, and the others have contributed. That's why we exist. That's why even our website is 
+      open source (click the "Fix an Issue" link in the footer, you'll see our website source code). Travis even <a href="https://lists.librenetwork.org/pipermail/discussion_lists.librenetwork.org/2019-April/000000.html"
+      target="_blank">posted about this</a> in the discussion mailing list.</p>
+      <p>We hope you decide to join us. If you do, please introduce yourself on the <a href="http://lists.librenetwork.org/mailman/listinfo/discussion_lists.librenetwork.org" target="_blank">discussion mailing list!</a></p>
 
 
     </div> <!-- /container -->
