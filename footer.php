f45304e099bf4b78bc1b36fe9b0df3eca08756d9
Travis Newton <tnewton1@gmail.com>
2021-03-10 20:30:28 -0500

    Add cpanel origin

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
     <div id="footer">
           <div class="container">
-          <p class="muted">&copy; <?php echo(date('Y')); ?> LibreNetwork Project.&nbsp;&nbsp;| <?php echo "Page last modified: " . date ("F d Y H:i:s.", getlastmod()); ?> | <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="/images/by-nc-sa-4.0-80x15.png" /></a><br><a href="/codeofconduct.php">Code of Conduct</a> | <a href="https://issues.librenetwork.org/bug_report_page.php">Report an Issue</a> | <a href="https://github.com/LibreNetwork/librenetwork.org">Fix an Issue</a></p>
+          <p class="muted">&copy; <?php echo(date('Y')); ?> LibreNetwork Project.&nbsp;&nbsp;| <?php echo "Page last modified: " . date ("F d Y H:i:s.", getlastmod()); ?> | <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="/images/by-nc-sa-4.0-80x15.png" /></a><br><a href="/codeofconduct.php">Code of Conduct</a> | <a href="https://issues.librenetwork.org/bug_report_page.php">Report an Issue</a> | <a href="https://github.com/LibreNetwork/librenetwork.org">Fix an Issue</a> | Open Source!</p>
           </div>
 	  
     </div>
